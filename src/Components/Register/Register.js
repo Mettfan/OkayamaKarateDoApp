f6692b9b6db6cf3c5848dab2bd8f7ee5fdc9cf16
Mettfan <cesaryannick6@gmail.com>
2022-08-25 01:10:40 -0500

    Adding login and register

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createOneUser } from '../../features/users/userSlice';
 import './Register.css'
 function Register() {
+    let dispatch = useDispatch()
+    let [user, setUser] = useState({
+        name: '',
+        age: null,
+        email: '',
+        password: '',
+        confirmPassword: '',
+        privileges: 'usuario'
+    })
     let handleRegisterSubmit = (e) => {
         e.preventDefault && e.preventDefault()
+        dispatch(createOneUser({user}))
 
+    }
+    let handleOnChange = (e) => {
+        console.log(user);
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
     }
     return ( <>
         <div className='registerContainer'>
@@ -11,16 +30,24 @@ function Register() {
             
             <form className='formContainer' onSubmit={(e)=>{handleRegisterSubmit(e)}}>
                 <div className='inputGroup'>
+                    <label>Nombre</label>
+                    <input name='name' onChange = { (e) => handleOnChange(e)}/>
+                </div>
+                <div className='inputGroup'>
+                    <label>Edad</label>
+                    <input name='age' type={'number'} onChange = { (e) => handleOnChange(e)}/>
+                </div>
+                <div className='inputGroup'>
                     <label>E-mail</label>
-                    <input name='email'/>
+                    <input name='email' onChange = { (e) => handleOnChange(e)}/>
                 </div>
                 <div className='inputGroup'>
                     <label>Contraseña</label>
-                    <input name='password' type={'password'}/>
+                    <input name='password' type={'password'} onChange = { (e) => handleOnChange(e)}/>
                 </div>
                 <div className='inputGroup'>
                     <label>Confirmar Contraseña</label>
-                    <input name='confirmPassword' type={'password'}/>
+                    <input name='confirmPassword' type={'password'} onChange = { (e) => handleOnChange(e)}/>
                 </div>
                 <button className='registerButton' >Register</button>
 
