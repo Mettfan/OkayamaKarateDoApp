368bf738c77e76a692da456354b3e875bff41953
Mettfan <cesaryannick6@gmail.com>
2022-08-31 12:46:26 -0500

    adding some fixes

--- src/Components/UncompletePage/UncompletePage.js
+++ src/Components/UncompletePage/UncompletePage.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import workingProgrammers from '../../Assets/workingProgrammers.png'
 import './Uncomplete.css'
 function UncompletePage() {
 
+
     return ( <>
     
         <div className='wpContainer'>
+            <b>{ window.location.pathname +' Estará listo pronto'}</b>
             <img src={workingProgrammers}></img>
             <b>Programadores Trabajando ... </b>
 
